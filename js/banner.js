a35ad3a886e4d41a204fcb8726febc3cd9727f1d
xsiry <xsiries@gmail.com>
2017-04-10 10:57:58 +0800

    #调整

---
+++ js/banner.js
@@ -0,0 +1,33 @@
+function actionUrl() {
+  // 初始化数据对象
+  var data = {
+    type: "",
+    list: []
+  };
+
+  var _fndname = decodeURI(getParams("class"));
+  if (_fndname == 'undefined') _fndname = '默认';
+  $.each(dataSource, function(i, o) {
+    if (o.type == _fndname) {
+      data = o;
+      return false;
+    }
+  })
+  var html = template('banner_template', data);
+  document.getElementById('content').innerHTML = html;
+}
+// url传参数变化类别
+function getParams(fndname) {
+  var url = location.search; //一般的查询
+  var query = url.substr(url.indexOf("?") + 1);
+  var pairs = query.split("&"); //在逗号处断开
+  for (var i = 0; i < pairs.length; i++) {
+    var pos = pairs[i].indexOf('='); //查找name=value
+    if (pos == -1)
+      continue; //如果没有找到就跳过
+    var argname = pairs[i].substring(0, pos); //提取name
+    var value = pairs[i].substring(pos + 1); //提取value
+    if (argname == fndname)
+      return value;
+  }
+}
