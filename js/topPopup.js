a870a27b4d96ae18cce49dabbf6d26fe006cf997
xsiry <xsiries@gmail.com>
2017-04-11 20:19:27 +0800

    #运行游戏弹窗

---
+++ js/topPopup.js
@@ -0,0 +1,78 @@
+var num = 0;
+var sec = 6;
+var interval;
+
+
+$(document).ready(function() {
+  init();
+  loadUpload();
+});
+
+function init() {
+  var width = $(window).width();
+  var height = $(window).height();
+  templateSource.imgConfig = {
+    width: width,
+    height: height
+  };
+  var html = template('top_popup_template', templateSource);
+  $('.content').html(html);
+  $('.num').text(templateSource.list.length);
+  interval = setInterval(queueingArr, sec * 1000);
+
+  $('.progress_bottom_win').css('-webkit-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+  $('.progress_bottom_win').css('-moz-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+  $('.progress_bottom_win').css('-o-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+  $('.progress_bottom_win').css('-ms-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+  $('.progress_bottom_win').css('animation', 'progress_bottom_act ' + sec + 's infinite linear');
+
+  $('.top_popup').hover(function() {
+    clearInterval(interval);
+    $('.left_btn').css('transform', 'translate(0px,0px)');
+    $('.right_btn').css('transform', 'translate(0px,0px)');
+    $('.progress_bottom_win').css('-webkit-animation', '');
+    $('.progress_bottom_win').css('-moz-animation', '');
+    $('.progress_bottom_win').css('-o-animation', '');
+    $('.progress_bottom_win').css('-ms-animation', '');
+    $('.progress_bottom_win').css('animation', '');
+  }, function() {
+    interval = setInterval(queueingArr, sec * 1000);
+    $('.left_btn').css('transform', 'translate(-60px,0px)');
+    $('.right_btn').css('transform', 'translate(60px,0px)');
+    $('.progress_bottom_win').css('-webkit-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+    $('.progress_bottom_win').css('-moz-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+    $('.progress_bottom_win').css('-o-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+    $('.progress_bottom_win').css('-ms-animation', 'progress_bottom_act ' + sec + 's infinite linear');
+    $('.progress_bottom_win').css('animation', 'progress_bottom_act ' + sec + 's infinite linear');
+  });
+
+  $('.left_btn').on('click', function() {
+    num = num - 1;
+    if (num < 0) num = templateSource.list.length - 1;
+    $('.img_a').hide();
+    $('.img_a').css('-webkit-animation', '');
+    $('.img_a_' + num).show();
+    $('.img_a_' + num).css('-webkit-animation', 'img_act 1s linear forwards');
+    $('.current').text(num + 1);
+  })
+
+  $('.right_btn').on('click', function() {
+    num = num + 1;
+    if (num >= templateSource.list.length) num = 0;
+    $('.img_a').hide();
+    $('.img_a').css('-webkit-animation', '');
+    $('.img_a_' + num).show();
+    $('.img_a_' + num).css('-webkit-animation', 'img_act 1s linear forwards');
+    $('.current').text(num + 1);
+  })
+}
+
+function queueingArr() {
+  num++;
+  if (num >= templateSource.list.length) num = 0;
+  $('.img_a').hide();
+  $('.img_a').css('-webkit-animation', '');
+  $('.img_a_' + num).show();
+  $('.img_a_' + num).css('-webkit-animation', 'img_act 1s linear forwards');
+  $('.current').text(num + 1);
+}
