5f5152ad4a3b0cdac3503d3df4980c9c6d69ae82
xsiry <xsiries@gmail.com>
2017-05-05 17:09:08 +0800

    #增加数据为空的容错性

--- js/rightLiftWindow.js
+++ js/rightLiftWindow.js
@@ -1,5 +1,8 @@
 var num = 0;
-var sec = templateSource.imgSec;
+var deNum = 4;
+var numTotal = templateSource.imgShowNum || ((templateSource.list.length < deNum) ? templateSource.list.length : deNum);
+var sec = templateSource.imgSec || 4;
+var imgRandom = templateSource.imgRandom || false;
 var interval;
 
 
@@ -10,7 +13,6 @@ $(document).ready(function() {
 });
 
 function init() {
-  var type = Math.floor(Math.random()*3);
   var width = $(window).width();
   var height = $(window).height();
 
@@ -28,19 +30,34 @@ function init() {
     list: []
   }
 
-  if (type == 0) {
-    tSource.iframe = templateSource.iframe;
-  }else if(type == 1) {
-    tSource.flash = templateSource.flash;
-  }else if(type == 2){
-    var rList = [];
-    var sList = templateSource.list;
-    if (templateSource.imgRandom == true) {
-      sList = sList.shuffle();
-    }
-    rList = sList.slice(0, templateSource.imgShowNum)
-    tSource.list = rList;
+  if (!templateSource.iframe && !templateSource.iframe.url && !templateSource.flash && !templateSource.flash.url && !templateSource.list && templateSource.list.length == 0) {
+    return;
   }
+
+  var status = true;
+  do {
+    var type = Math.floor(Math.random()* 3);
+    if (type == 0) {
+      status = (templateSource.iframe && templateSource.iframe.url);
+      if (status) tSource.iframe = templateSource.iframe;
+    }else if(type == 1) {
+      status = (templateSource.flash && templateSource.flash.url);
+      if (status) tSource.flash = templateSource.flash;
+    }else if(type == 2){
+      var rList = [];
+      var sList = templateSource.list;
+      status = (sList) && (sList.length > 0);
+      if (status) {
+        if (imgRandom == true) {
+          sList = sList.shuffle();
+        }
+
+        rList = sList.slice(0, numTotal)
+        tSource.list = rList;
+      }
+    }
+  }
+  while (!status);
 
   var html = template('lift_win_template', tSource);
   $('.content').html(html);
@@ -71,7 +88,7 @@ function init() {
 
 function queueingArr() {
   num++;
-  if (num >= templateSource.imgShowNum) num = 0;
+  if (num >= numTotal) num = 0;
   $('.img_a').hide();
   $('.img_a').css('-webkit-animation', '');
   $('.img_a').css('-moz-animation', '');
--- js/topPopup.js
+++ js/topPopup.js
@@ -1,5 +1,8 @@
 var num = 0;
-var sec = templateSource.imgSec;
+var deNum = 4;
+var numTotal = templateSource.imgShowNum || ((templateSource.list.length < deNum) ? templateSource.list.length : deNum);
+var sec = templateSource.imgSec || 4;
+var imgRandom = templateSource.imgRandom || false;
 var interval;
 
 
@@ -10,7 +13,6 @@ $(document).ready(function() {
 });
 
 function init() {
-  var type = Math.floor(Math.random()*3);
   var width = $(window).width();
   var height = $(window).height();
 
@@ -28,23 +30,39 @@ function init() {
     list: []
   }
 
-  if (type == 0) {
-    tSource.iframe = templateSource.iframe;
-  }else if(type == 1) {
-    tSource.flash = templateSource.flash;
-  }else if(type == 2){
-    var rList = [];
-    var sList = templateSource.list;
-    if (templateSource.imgRandom == true) {
-      sList = sList.shuffle();
-    }
-    rList = sList.slice(0, templateSource.imgShowNum)
-    tSource.list = rList;
+  if (!templateSource.iframe && !templateSource.iframe.url && !templateSource.flash && !templateSource.flash.url && !templateSource.list && templateSource.list.length == 0) {
+    return;
   }
+
+  var status = true;
+  do {
+    var type = Math.floor(Math.random()* 3);
+    if (type == 0) {
+      status = (templateSource.iframe && templateSource.iframe.url);
+      if (status) tSource.iframe = templateSource.iframe;
+    }else if(type == 1) {
+      status = (templateSource.flash && templateSource.flash.url);
+      if (status) tSource.flash = templateSource.flash;
+    }else if(type == 2){
+      var rList = [];
+      var sList = templateSource.list;
+      status = (sList) && (sList.length > 0);
+      if (status) {
+        if (imgRandom == true) {
+          sList = sList.shuffle();
+        }
+
+        rList = sList.slice(0, numTotal)
+        tSource.list = rList;
+      }
+    }
+  }
+  while (!status);
+
 
   var html = template('top_popup_template', tSource);
   $('.content').html(html);
-  $('.num').text(templateSource.imgShowNum);
+  $('.num').text(numTotal);
   interval = setInterval(queueingArr, sec * 1000);
 
   $('.progress_bottom_win').css('-webkit-animation', 'progress_bottom_act ' + sec + 's infinite linear');
@@ -75,20 +93,20 @@ function init() {
 
   $('.left_btn').on('click', function() {
     num = num - 1;
-    if (num < 0) num = templateSource.imgShowNum - 1;
+    if (num < 0) num = numTotal - 1;
     choseImg(num);
   })
 
   $('.right_btn').on('click', function() {
     num = num + 1;
-    if (num >= templateSource.imgShowNum) num = 0;
+    if (num >= numTotal) num = 0;
     choseImg(num);
   })
 }
 
 function queueingArr() {
   num++;
-  if (num >= templateSource.imgShowNum) num = 0;
+  if (num >= numTotal) num = 0;
   choseImg(num);
 }
 
