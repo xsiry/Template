aa53c9a6e7076943071de225157dbe60820f25f3
xsiry <xsiries@gmail.com>
2017-05-05 14:09:03 +0800

    #游戏运行弹窗增加flash、外网，并随机

--- js/topPopup.js
+++ js/topPopup.js
@@ -1,23 +1,50 @@
 var num = 0;
-var sec = 6;
+var sec = templateSource.imgSec;
 var interval;
 
 
 $(document).ready(function() {
+  arrRandom();
   init();
   loadUpload();
 });
 
 function init() {
+  var type = Math.floor(Math.random()*3);
   var width = $(window).width();
   var height = $(window).height();
-  templateSource.imgConfig = {
-    width: width,
-    height: height
-  };
-  var html = template('top_popup_template', templateSource);
+
+  tSource = {
+    imgConfig: {
+      width: width,
+      height: height
+    },
+    iframe: {
+      url: ''
+    },
+    flash: {
+      url: ''
+    },
+    list: []
+  }
+
+  if (type == 0) {
+    tSource.iframe = templateSource.iframe;
+  }else if(type == 1) {
+    tSource.flash = templateSource.flash;
+  }else if(type == 2){
+    var rList = [];
+    var sList = templateSource.list;
+    if (templateSource.imgRandom == true) {
+      sList = sList.shuffle();
+    }
+    rList = sList.slice(0, templateSource.imgShowNum)
+    tSource.list = rList;
+  }
+
+  var html = template('top_popup_template', tSource);
   $('.content').html(html);
-  $('.num').text(templateSource.list.length);
+  $('.num').text(templateSource.imgShowNum);
   interval = setInterval(queueingArr, sec * 1000);
 
   $('.progress_bottom_win').css('-webkit-animation', 'progress_bottom_act ' + sec + 's infinite linear');
@@ -48,20 +75,20 @@ function init() {
 
   $('.left_btn').on('click', function() {
     num = num - 1;
-    if (num < 0) num = templateSource.list.length - 1;
+    if (num < 0) num = templateSource.imgShowNum - 1;
     choseImg(num);
   })
 
   $('.right_btn').on('click', function() {
     num = num + 1;
-    if (num >= templateSource.list.length) num = 0;
+    if (num >= templateSource.imgShowNum) num = 0;
     choseImg(num);
   })
 }
 
 function queueingArr() {
   num++;
-  if (num >= templateSource.list.length) num = 0;
+  if (num >= templateSource.imgShowNum) num = 0;
   choseImg(num);
 }
 
@@ -79,4 +106,20 @@ function choseImg(num) {
   $('.img_a_' + num).css('-ms-animation', 'img_act 1s linear forwards');
   $('.img_a_' + num).css('animation', 'img_act 1s linear forwards');
   $('.current').text(num + 1);
+}
+
+function arrRandom() {
+  if (!Array.prototype.shuffle) {
+    Array.prototype.shuffle = function() {
+      for (var j, x, i = this.length; i; j = parseInt(Math.random() * i), x = this[--i], this[i] = this[j], this[j] = x);
+
+      // for (var i = 0; i < this.length; i) {
+      //   var j = parseInt(Math.random() * i);
+      //   var x = this[i];
+      //   this[i++] = this[j];
+      //   this[j] = x;
+      // }
+      return this;
+    };
+  }
 }
