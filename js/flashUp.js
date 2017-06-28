446ba5d1bf259d9b459208549e761aa1cfdb392f
xsiry <xsiries@gmail.com>
2017-06-28 18:47:25 +0800

    #flash上报模板

---
+++ js/flashUp.js
@@ -0,0 +1,57 @@
+var _class = decodeURI(getParams("class"));
+
+$(document).ready(function() {
+  init();
+  loadUpload();
+});
+
+function init() {
+  var tsource = {
+    flash: {
+      url: '',
+      clickUrl: '',
+      adId: '',
+      gameId: ''
+    },
+    img: {
+      url: '',
+      clickUrl: '',
+      adId: '',
+      gameId: ''
+    }
+  }
+  $.each(source, function(i, o) {
+    if (o.type == _class) {
+      tsource.flash = o.flash;
+      tsource.img = o.img;
+    }
+  })
+  var html = template('flash_template', tsource);
+  $('.content').html(html);
+
+  if (tsource.flash.url != '') {
+    var ucid = $('#ucid').val();
+    var area = $('#area').val();
+    var mac  = $('#mac').val();
+    var adId = $('.up_flash_a').data('adid');
+    var gameId = $('.up_flash_a').data('gameid');
+    var params = [10000, ucid, area, mac, adId, gameId, 1, 0].join(',');
+    upload(params);
+  }
+}
+
+// url传参数变化类别
+function getParams(fndname) {
+  var url = location.search; //一般的查询
+  var query = url.substr(url.indexOf("?") + 1);
+  var pairs = query.split("&"); //在逗号处断开
+  for (var i = 0; i < pairs.length; i++) {
+    var pos = pairs[i].indexOf('='); //查找name=value
+    if (pos == -1)
+      continue; //如果没有找到就跳过
+    var argname = pairs[i].substring(0, pos); //提取name
+    var value = pairs[i].substring(pos + 1); //提取value
+    if (argname == fndname)
+      return value;
+  }
+}
--- js/upload.js
+++ js/upload.js
@@ -8,17 +8,24 @@ function loadUpload() {
   var mac  = $('#mac').val();
   $('.up_img').each(function(i, img) {
     $(this).closest('.up_a').on('click', function(e) {
-      var adId = $(e.currentTarget).attr('adId');
-      var gameId = $(e.currentTarget).attr('gameId');
+      var adId = $(e.currentTarget).data('adid');
+      var gameId = $(e.currentTarget).data('gameid');
       clickUpload(adId, gameId);
     })
+  })
+
+  $('.up_flash_a').on('click', function(e) {
+    var adId = $(e.currentTarget).data('adid');
+    var gameId = $(e.currentTarget).data('gameid');
+    clickUpload(adId, gameId);
   })
 
   $(document).ready(function () {
     var paramsArr = [];
     $('.up_img').each(function(i, img) {
-      var adId = $(img).attr('adId');
-      var gameId = $(img).attr('gameId');
+      var adId = $(img).data('adid');
+      var gameId = $(img).data('gameid');
+      // 加载上报
       var params = [10000, ucid, area, mac, adId, gameId, 1, 0].join(',');
       paramsArr.push(params);
     });
@@ -30,6 +37,7 @@ function clickUpload(adId, gameId) {
   var ucid = $('#ucid').val();
   var area = $('#area').val();
   var mac  = $('#mac').val();
+  // 点击上报
   var params = ["10000", ucid, area, mac, adId, gameId, 0, 1].join(',');
   upload(params)
 }
